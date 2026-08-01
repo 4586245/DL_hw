9f02405688234b7a3fa4ec901e13f3186feca5a9
Polina Grebennikova <pn.grebennikova@gmail.com>
2026-08-01 07:42:12 +0300

    LCNN with MFM also according to paper

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,4 +1,5 @@
 from src.model.baseline_model import BaselineModel
+from src.model.lcnn import LCNN
 
 __all__ = [
     "BaselineModel",
---
+++ src/model/lcnn.py
@@ -0,0 +1,78 @@
+import torch
+from torch import nn
+
+
+# mfm was used in STC ... for the ASVspoof2019 paper
+class MFM(nn.Module):
+    def __init__(self, dim=1):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        a, b = torch.chunk(x, 2, dim=self.dim)
+        return torch.max(a, b)
+
+
+class LCNN(nn.Module):
+    def __init__(self, in_freq=257, compressed=60, dropout=0.75, n_class=2):
+        super().__init__()
+        self.freq_compress = nn.Linear(in_freq, compressed)
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=5, stride=1, padding=2),
+            MFM(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(32, 64, kernel_size=1, stride=1),
+            MFM(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 96, kernel_size=3, stride=1, padding=1),
+            MFM(),
+            nn.MaxPool2d(2, 2),
+            nn.BatchNorm2d(48),
+            nn.Conv2d(48, 96, kernel_size=1, stride=1),
+            MFM(),
+            nn.BatchNorm2d(48),
+            nn.Conv2d(48, 128, kernel_size=3, stride=1, padding=1),
+            MFM(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(64, 128, kernel_size=1, stride=1),
+            MFM(),
+            nn.BatchNorm2d(64),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            MFM(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, kernel_size=1, stride=1),
+            MFM(),
+            nn.BatchNorm2d(32),
+            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
+            MFM(),
+            nn.MaxPool2d(2, 2),
+        )
+
+        self.head = nn.Sequential(
+            nn.Linear(32 * 3 * 46, 160),
+            MFM(),
+            nn.Dropout(dropout),
+            nn.BatchNorm1d(80),
+            nn.Linear(80, n_class),
+        )
+
+    def forward(self, data_object, **batch):
+        x = data_object
+        x = x.transpose(1, 2)  # (batch, 750, 257)
+        x = self.freq_compress(x)  # (batch, 750, 60)
+        x = x.transpose(1, 2)  # (batch, 60, 750)
+
+        x = x.unsqueeze(1)  # (batch, 1, 60, 750)
+        x = self.conv(x)  # (batch, 32, 3, 46)
+        x = x.flatten(1)  # (batch, 4416)
+        logits = self.head(x)  # (batch, 2)
+        return {"logits": logits}
+
+    def __str__(self):
+        params = sum([p.numel() for p in self.parameters()])
+        train_params = sum([p.numel() for p in self.parameters() if p.requires_grad])
+
+        res = super().__str__()
+        res = res + f"\nAll parameters: {params}"
+        res = res + f"\nTrainable parameters: {train_params}"
+        return res
